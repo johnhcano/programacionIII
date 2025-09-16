8b375dd360dce0638ee33ec0d5e778c9baca0542
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-09-16 16:06:43 +0000

    Pilas en java

---
+++ 22025-S441A/Clase7/PilaS441.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class PilaS441 {
+    public static void main(String[] args) {
+        
+        //Creación de una pila de números enteros
+        Stack<Integer> pila = new Stack<>();
+
+        //Insertar elementos en la pila
+        pila.push(8);
+        pila.push(4);
+        pila.push(9);
+        pila.push(10);
+        pila.push(6);
+
+        //Imprimir la pila
+        System.out.println(pila);
+
+        //Mostrar el elemento que está en el tope de la pila sin removerlo
+        System.out.println("Tope de la pila: " + pila.peek());
+
+        //Remover el elemento que se encuentra en el tope de la pila
+        System.out.println("Elemento removido del tope de la pila: " + pila.pop());
+
+        //Imprimir la pila
+        System.out.println(pila);
+    }
+}
