4792538150494b6d80c44cb1606e050e0d083846
JhonHCanoB <jhon.canob@campusucc.edu.co>
2026-03-04 16:05:28 +0000

    se agregaron los comentarios de cada sentencia

--- 12026-4303A-2/Clase5/Matrices.java
+++ 12026-4303A-2/Clase5/Matrices.java
@@ -1,9 +1,13 @@
 public class Matrices {
     public static void main(String[] args) {
 
+            //Creación de la matriz
+            
             int[][] m = {{9, 4, 1},
                          {2, 8, 7},
                          {3, 5, 6}};
+
+            //Mostrar las posiciones y los elementos de la matriz
 
             for (int i = 0; i < m.length; i++) {
                 for (int j = 0; j < m[0].length; j++) {
