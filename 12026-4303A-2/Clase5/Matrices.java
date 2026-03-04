f9d621615338fe4fae03bd5b46b6b5f510932143
JhonHCanoB <jhon.canob@campusucc.edu.co>
2026-03-04 16:03:08 +0000

    ejemplo1

--- 12026-4303A-2/Clase5/Matrices.java
+++ 12026-4303A-2/Clase5/Matrices.java
@@ -1,6 +1,15 @@
 public class Matrices {
     public static void main(String[] args) {
-        
+
+            int[][] m = {{9, 4, 1},
+                         {2, 8, 7},
+                         {3, 5, 6}};
+
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[0].length; j++) {
+                    System.out.println("m[" + i + "][" + j + "]=" + m[i][j]);
+                }
+            }
         }   
 }
         
