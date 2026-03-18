87ae5dcabb841cd3316152ae3a5315397a5c77ed
JhonHCanoB <jhon.canob@campusucc.edu.co>
2026-03-18 17:14:35 +0000

    ok

--- 12026-4303A-2/Clase7/Pilas.java
+++ 12026-4303A-2/Clase7/Pilas.java
@@ -1,29 +1,41 @@
+import java.util.Comparator;
 import java.util.Stack;
 
 public class Pilas {
     public static void main(String[] args) {
         
         Stack<String> pila = new Stack<>();
+        
         //Mostrar si la pila está vacía
         System.out.println(pila.empty()); //true
+        
         //Agregar elementos a la pila
         pila.push("Sebastian");
         pila.push("Alejandro");
         pila.push("Natalia");
         pila.push("Juan");
+        
         //Mostrar el contenido de la pila
         System.out.println(pila); //[Sebastian, Alejandro, Natalia, Juan]
+        
         //Mostrar el tope de la pila sin removerlo
         System.out.println(pila.peek()); // Juan
+        
         //Mostrar el contenido de la pila
         System.out.println(pila); //[Sebastian, Alejandro, Natalia, Juan]
+        
         //Eliminar el elemento que se encuentra en el tope de la pila
         System.out.println("Se eliminó: " + pila.pop()); //Juan
+        
         //Mostrar el contenido de la pila
         System.out.println(pila); //[Sebastian, Alejandro, Natalia]
+        
         //Posiciones de los elementos dentro de la pila
         System.out.println(pila.search("Sebastian")); //3
         System.out.println(pila.search("Alejandro")); //2
         System.out.println(pila.search("Natalia")); //1
+
+        //Tamaño de la pila
+        System.out.println("Tamaño de la pila: " + pila.size()); //3
     }    
 }
